7130b4f442781023c6d941dc6f91c229bcb8daa5
Frostylele21 <79452790+Frostylele21@users.noreply.github.com>
2022-03-15 00:24:42 +0100

    14.03.2022

--- PHP/zad_php2/zad3.php
+++ PHP/zad_php2/zad3.php
@@ -1,17 +1,19 @@
 <form method="GET">
 <select name="podatki">
-<option value="23,9">Liniowy</option>
+<option value="23.9">Liniowy</option>
 <option value="14">Ryczałt</option>
 <option value="32">Progresywny</option>
-<option value="Karta Podatkowa">Karta Podatkowa</option>
 </select>
-<input type="submit" name="podatki"></input>
+<input name="liczba1"> <-- Podaj kwotę</input> 
+<input type="submit" name="podatki" && name="liczba1"></input>
 </form>
 
 <?php
-if(isset($_GET['podatki']))
+if(isset($_GET['podatki']) && ($_GET['liczba1']))
 {
-$podatek = $_GET['podatki'];
-$podatek 
+$typ = intval($_GET['podatki']);
+$kwota = $_GET['liczba1'];
+$podatek = $kwota - (($kwota / 100)*$typ);
+echo "Dla podanej kwoty podatek wynosi: ".$podatek;
 }
 ?>
