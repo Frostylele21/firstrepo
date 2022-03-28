dbf610e930649d1289a8cee6eeaef096b2b0f4aa
Frostylele21 <79452790+Frostylele21@users.noreply.github.com>
2022-03-28 21:21:53 +0200

    28.03.2022

--- PHP/zad_php4/zad1.php
+++ PHP/zad_php4/zad1.php
@@ -1,14 +1,9 @@
 <form method="POST">
-<input name="pin"> Podaj pin do karty </input>
-<input name="kwota"> Podaj kwotę </input> /*zrobic dedykowane przyciski na wybrane kwoty, mozliwosc wpisania wlasnej kwoty w nominalach bankomatowych (10,20,50 jesli inne wyrzuca blad), wyswietlenie inofmracji o wyplaconej kwocie.*/
+<input name="pin"> Podaj numer PIN do karty. </input>
 <input type="submit" name="logowanie"></input>
 </form>
 
 <?php
-function wyplac(){
-    echo "Wypłaciłeś pieniądze";
-}
-
 if(isset($_POST['logowanie']))
 {
     $liczba1 = $_POST['pin'];
@@ -18,9 +13,34 @@ if(isset($_POST['logowanie']))
         echo '<div>
         <h1> Zalogowałeś się poprawnie! </h1>
         <h2> Witamy w banku XYZ! </h2>
-        <p> Wybierz co chcesz zrobić: </p></div>';
+        <p> Wybierz kwotę do wypłaty: </p></div>
+        <input type="button" value="10zł"></input>
+        <input type="button" value="20zł"></input>
+        <input type="button" value="50zł"></input>
+        <input type="button" value="100zł"></input>
+        <input type="button" value="200zł"></input>
+        <input type="button" value="500zł"></input>
+        <br>
+        <br>
+        <h4> Lub wpisz własną kwotę poniżej:
+        <br>
+        <br>
+        <input name="kwota"> WYPŁATA ŚRODKÓW (Podaj kwotę!): </input>
+        <input type="submit" name="wyplata"></input>';
     }
     else
-    echo "Podany pin jest nieprawidłowy. Spróbuj ponownie.";
+        echo "Podany pin jest nieprawidłowy. Spróbuj ponownie.";
 }
+    if(isset($_POST['logowanie']))
+        {
+            if(isset($_POST['wyplata']))
+                {
+                    $liczba2 = $_POST['kwota'];
+                    echo "Wypłacono ".$liczba2." złotych. Dziękujemy za skorzystanie z naszych bankomatów!";
+                }
+            else
+                echo "";
+        }
+    else 
+        echo "";
 ?>
